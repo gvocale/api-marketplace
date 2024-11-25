9632cfbf8bf91def6a5c26af5670bbcdf41ee87d
Giovanni <1319291+gvocale@users.noreply.github.com>
2024-11-25 17:28:50 -0500

    add redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // eslint: { ignoreDuringBuilds: true },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/products/transfer-funds",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
