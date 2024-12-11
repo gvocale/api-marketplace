28bd9c099c7876b60ba120fa823098b68a3a456f
Giovanni <1319291+gvocale@users.noreply.github.com>
2024-12-11 16:54:05 -0500

    add different capitals on world

--- src/app/features/transfer-funds/components/Globe/index.tsx
+++ src/app/features/transfer-funds/components/Globe/index.tsx
@@ -7,7 +7,10 @@ import {
   useState,
 } from "react";
 import { GlobeMethods, GlobeProps } from "react-globe.gl";
+import * as THREE from "three";
 import * as interRegular from "./inter-regular.json";
+// import * as d3 from "d3";
+import { CAPITAL_CITIES } from "./data";
 
 interface GlobeLabelData {
   lat: number;
@@ -24,6 +27,10 @@ const GlobeForwarded = forwardRef<GlobeMethods, GlobeProps>((props, ref) => (
 
 GlobeForwarded.displayName = "GlobeForwarded";
 
+const CLOUDS_IMG_URL = "/transfer-funds/images/clouds.png";
+const CLOUDS_ALT = 0.001;
+const CLOUDS_ROTATION_SPEED = -0.001; // deg/frame
+
 export function Globe() {
   const globeRef = useRef<GlobeMethods | null>(null);
   const [globeReady, setGlobeReady] = useState(false);
@@ -33,16 +40,11 @@ export function Globe() {
   const [globeHeight, setGlobeHeight] = useState(0);
 
   const props: GlobeProps = {
-    labelsData: [
-      { name: "$ USD", lat: 40.7128, lng: -74.006 }, // New York
-      { name: "€ EUR", lat: 51.5074, lng: -0.1278 }, // London
-      { name: "¥ JPY", lat: 35.6762, lng: 139.6503 }, // Tokyo
-      { name: "¥ CNY", lat: 31.2304, lng: 121.4737 }, // Shanghai
-      { name: "₣ CHF", lat: 47.3769, lng: 8.5417 }, // Zurich
-      { name: "$ SGD", lat: 1.3521, lng: 103.8198 }, // Singapore
-      { name: "$ AUD", lat: -33.8688, lng: 151.2093 }, // Sydney
-      { name: "$ HKD", lat: 22.3193, lng: 114.1694 }, // Hong Kong
-    ],
+    labelsData: CAPITAL_CITIES.map((city) => ({
+      name: `${city.currency} ${city.city}`,
+      lat: city.lat,
+      lng: city.lng,
+    })),
     // @ts-expect-error GlobeProps is not typed
     labelLat: (d: GlobeLabelData) => d.lat,
     // @ts-expect-error GlobeProps is not typed
@@ -72,7 +74,29 @@ export function Globe() {
     globeRef.current.controls().enableZoom = false;
     globeRef.current.controls().autoRotate = true;
     globeRef.current.controls().autoRotateSpeed = 1;
-  }, [globeReady]);
+
+    new THREE.TextureLoader().load(CLOUDS_IMG_URL, (cloudsTexture) => {
+      if (!globeRef.current) return;
+      const clouds = new THREE.Mesh(
+        new THREE.SphereGeometry(
+          globeRef.current.getGlobeRadius() * (1 + CLOUDS_ALT),
+          75,
+          75
+        ),
+        new THREE.MeshPhongMaterial({
+          map: cloudsTexture,
+          transparent: true,
+          opacity: 0.25,
+        })
+      );
+      globeRef.current.scene().add(clouds);
+
+      (function rotateClouds() {
+        clouds.rotation.y += (CLOUDS_ROTATION_SPEED * Math.PI) / 180;
+        requestAnimationFrame(rotateClouds);
+      })();
+    });
+  }, [globeReady, globeRef]);
 
   useEffect(() => {
     if (!globeContainerRef.current) return;
@@ -92,18 +116,46 @@ export function Globe() {
     };
   }, []);
 
+  // const [airports, setAirports] = useState([]);
+  // const [routes, setRoutes] = useState([]);
+  // const [hoverArc, setHoverArc] = useState();
+
   return (
     <div style={{ width: "100%", height: "100%" }} ref={globeContainerRef}>
       <GlobeForwarded
         ref={globeRef}
         {...props}
+        // arcsData={routes}
+        // arcLabel={(d) => `${d.airline}: ${d.srcIata} &#8594; ${d.dstIata}`}
+        // arcStartLat={(d) => +d.srcAirport.lat}
+        // arcStartLng={(d) => +d.srcAirport.lng}
+        // arcEndLat={(d) => +d.dstAirport.lat}
+        // arcEndLng={(d) => +d.dstAirport.lng}
+        arcDashLength={0.4}
+        arcDashGap={0.2}
+        arcDashAnimateTime={1500}
+        arcsTransitionDuration={0}
+        // arcColor={(d) => {
+        //   const op = !hoverArc ? OPACITY : d === hoverArc ? 0.9 : OPACITY / 4;
+        //   return [`rgba(0, 255, 0, ${op})`, `rgba(255, 0, 0, ${op})`];
+        // }}
+        // onArcHover={setHoverArc}
+        // pointsData={airports}
+        pointColor={() => "orange"}
+        pointAltitude={0}
+        pointRadius={0.04}
+        pointsMerge={true}
         onGlobeReady={() => setGlobeReady(true)}
-        globeImageUrl="/transfer-funds/images/earth-dark.jpg"
+        // globeImageUrl="/transfer-funds/images/earth-blue-marble.jpg"
+        // globeImageUrl="/transfer-funds/images/earth-dark.jpg"
+        globeImageUrl="/transfer-funds/images/earth-night.jpg"
+        bumpImageUrl="/transfer-funds/images/earth-topology.png"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
         showAtmosphere={true}
         animateIn={true}
-        arcDashAnimateTime={180}
-        arcDashLength={150}
-        arcDashGap={250}
+        // arcDashAnimateTime={180}
+        // arcDashLength={150}
+        // arcDashGap={250}
         width={globeWidth}
         height={globeHeight}
       />
